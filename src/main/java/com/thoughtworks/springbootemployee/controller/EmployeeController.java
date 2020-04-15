54991b7be7b42c7f9259ad14533ccb843c205ba2
Kit Yan <kit.yan@oocl.com>
2020-04-15 18:22:05 +0800

    [Kit] Addded search-by-name

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,10 +1,8 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +12,24 @@ import java.util.List;
 public class EmployeeController {
     private List<Employee> employees = new ArrayList<>();
     @GetMapping
-    public List<Employee> getAll() {
-        employees.add(new Employee(0, "Xiaoming", 20, "Male"));
-        employees.add(new Employee(1, "Xiaohong", 19, "Female"));
-        employees.add(new Employee(2, "Xiaozhi", 15, "Male"));
-        employees.add(new Employee(3, "Xiaogang", 16, "Male"));
-        employees.add(new Employee(4, "Xiaoxia", 15, "Female"));
+    public List<Employee> getAllEmployee() {
         return employees;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addEmployee(@RequestBody Employee employee) {
+        if (employees.stream().anyMatch(employeeInList -> employeeInList.getName().equals(employee.getName()))) {
+            return "cannot add";
+        }
+        employee.setId(employees.size());
+        employees.add(employee);
+        return "added";
+    }
+
+    @PostMapping("/search-by-name")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Employee findEmployeeByName(@RequestBody String name) {
+        return employees.stream().filter(employee -> employee.getName().equals(name)).findFirst().orElse(null);
     }
 }
